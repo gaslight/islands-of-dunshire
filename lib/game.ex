61b02d5e594c122f46f76b2456622d1fb0f6d3ad
Stamates <john@teamgaslight.com>
2018-05-15 10:03:37 -0400

    determine if game is won

--- lib/game.ex
+++ lib/game.ex
@@ -1,8 +1,15 @@
 defmodule Islands.Game do
   alias __MODULE__
-  alias Islands.Guess
+  alias Islands.{Board, Guess}
 
-  def hit_or_miss?(board, %Guess{coordinates: coordinates} = guess) do
-    coordinates in board.islands
+  def hit_or_miss?(%Board{islands: islands}, %Guess{coordinates: coordinates} = guess) do
+    coordinates in islands
+  end
+
+  def winner?(%Board{islands: islands, hits: hits}) do
+    case Enum.reject(islands, fn(coordinates) -> coordinates in hits end) do
+      [] -> true
+      _ -> false
+    end
   end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -11,4 +11,15 @@ defmodule GameTest do
       refute Game.hit_or_miss?(%Board{islands: [{0, 0}]}, %Guess{coordinates: {0, 1}})
     end
   end
+
+  describe "winner?/1" do
+    test "processes winning game" do
+      assert Game.winner?(%Board{islands: [{0, 0}], hits: [{0, 0}]})
+    end
+
+    test "processes non winning game" do
+      refute Game.winner?(%Board{islands: [{0, 0}], hits: []})
+      refute Game.winner?(%Board{islands: [{0, 0}, {0, 1}], hits: [{0, 0}]})
+    end
+  end
 end
